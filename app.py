bba97fe7992fbda8a89260aa00770e73c75bbf1b
bogdan01m <152898065+bogdan01m@users.noreply.github.com>
2024-05-09 15:46:29 +0500

    app.py

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
-import plotly_express as px
 import torch
+from torch.utils.data import Dataset, DataLoader
 import copy
+import plotly_express as px
 import numpy as np
 import pandas as pd
 import seaborn as sns
@@ -11,6 +10,10 @@ import matplotlib.pyplot as plt
 from torch import nn, optim
 import torch.nn.functional as F
 import argparse
+import time
+from tqdm import tqdm
+import os
+from scipy.fft import fft
 from sklearn.preprocessing import MinMaxScaler
 import warnings
 warnings.filterwarnings('ignore')
@@ -22,26 +25,38 @@ device=torch.device('cpu')
 ####################################################################
 #здесь логика загрузки данных 
 ####################################################################    
-
-def create_dataset(dataframe):
-    df=dataframe[['ReservoirTemperature_c', 'MeasureMRM204', 'MeasureMRM205',
-       'ProducingGOR_m3_t', 'LiquidViscosity', 'MeasureMRM219',
-       'WeightedParticlesFactor_mg_l', 'MeasureMRM187', 'MeasureMRM188',
-       'MeasureMRM12', 'MeasureMRM30', 'MeasureMRM143', 'MeasureMRM144', ]]
-    
-    # # Создаем экземпляр MinMaxScaler
+def prepare_data(data):
+    df=data[['ReservoirTemperature_c', 'MeasureMRM204', 'MeasureMRM205',
+       'ProducingGOR_m3_t', 'LiquidViscosity', 'WeightedParticlesFactor_mg_l',
+       'MeasureMRM187', 'MeasureMRM188', 'MeasureMRM12', 'MeasureMRM30',
+       'MeasureMRM143', 'MeasureMRM144']]
+    data_fft = fft(df)
+    # Создаем экземпляр MinMaxScaler
     scaler = MinMaxScaler()
+    columns=df.columns
+    # Нормализуем данные в датафре  йме X
+    data_fft = pd.DataFrame(scaler.fit_transform(data_fft.real), columns=columns)
+    return data_fft, columns
+class Create_dataset(Dataset):
+    def __init__(self, data, seq_len=1, batch_size=1):
+        self.data = data
+        self.seq_len = seq_len
+        self.batch_size = batch_size
+        self.n_samples, self.n_features = self.data.shape
+        self.dataset = self.create_dataset(self.data, batch_size)
 
-    # Извлекаем названия колонок
-    columns = df.columns
+    def create_dataset(self, data, batch_size):
+        sequences = []
+        for i in range(len(data) - self.seq_len + 1):
+            sequence = data.iloc[i:i+self.seq_len].values.astype(np.float32)
+            sequences.append(torch.from_numpy(sequence))
+        return DataLoader(sequences, batch_size=batch_size, shuffle=False)
 
-    # Нормализуем данные в датафрейме X
-    series_normalized = pd.DataFrame(scaler.fit_transform(df), columns=columns)
-   
-    sequences = series_normalized.astype(np.float32).to_numpy().tolist()
-    dataset=[torch.tensor(s).unsqueeze(1) for s in sequences]
-    n_seq, seq_len, n_features=torch.stack(dataset).shape
-    return dataset, seq_len, n_features, series_normalized
+    def __getitem__(self, index):
+        return self.data.iloc[index]
+
+    def __len__(self):
+        return len(self.data) - self.seq_len + 1
 
 ####################################################################
 #Здесь модель
@@ -49,77 +64,86 @@ def create_dataset(dataframe):
 
 
 class Encoder(nn.Module):
-    def __init__(self, seq_len, n_features):
+    def __init__(self, seq_len, n_features, args):
         super(Encoder, self).__init__()
-
-        self.seq_len = seq_len
-        self.n_features = n_features
+        self.seq_len, self.n_features = seq_len, n_features
         self.hidden_dim = 2 * args.embedding_dim
-
-        self.rnn1 = nn.LSTM(
+        self.lstm1 = nn.LSTM(
             input_size=n_features,
             hidden_size=self.hidden_dim,
             num_layers=args.n_layers,
-            batch_first=True
-        )
-        self.rnn2 = nn.LSTM(
+            batch_first=True,
+            dtype=torch.float32
+        ).to(args.device)
+        self.lstm2 = nn.LSTM(
             input_size=self.hidden_dim,
             hidden_size=args.embedding_dim,
             num_layers=args.n_layers,
-            batch_first=True
-        )
+            batch_first=True,
+            dtype=torch.float32
+        ).to(args.device)
 
     def forward(self, x):
-        x, (_, _) = self.rnn1(x)
-        x, (hidden_n, _) = self.rnn2(x)
-        return hidden_n  # Возвращаем hidden_n без изменения формы
+        x = x.to(args.device).to(torch.float32)
+        batch_size = x.size(0)
+
+        # print(f'ENCODER input dim: {x.shape}')
+        x = x.reshape((batch_size, self.seq_len, self.n_features))
+        # print(f'ENCODER reshaped dim: {x.shape}')
+        x, (_, _) = self.lstm1(x)
+        # print(f'ENCODER output lstm1 dim: {x.shape}')
+        x, (hidden_n, _) = self.lstm2(x)
+        # print(f'ENCODER output lstm2 dim: {x.shape}')
+        # print(f'ENCODER hidden_n lstm2 dim: {hidden_n.shape}')
+        # print(f'ENCODER hidden_n wants to be reshaped to : {(batch_size, args.embedding_dim)}')
+        return hidden_n.reshape((batch_size, args.embedding_dim))
 
 class Decoder(nn.Module):
-    def __init__(self, seq_len, n_features):
+    def __init__(self, seq_len, n_features, args):
         super(Decoder, self).__init__()
-
-        self.seq_len = seq_len
-        self.hidden_dim = 2 * args.embedding_dim
-        self.n_features = n_features
-
-        self.rnn1 = nn.LSTM(
+        self.seq_len, self.input_dim = seq_len, args.embedding_dim
+        self.hidden_dim, self.n_features = 2 * args.embedding_dim, n_features
+        self.lstm1 = nn.LSTM(
             input_size=args.embedding_dim,
             hidden_size=args.embedding_dim,
-            num_layers=args.n_layers,
+            num_layers=1,
             batch_first=True
-        )
-        self.rnn2 = nn.LSTM(
+        ).to(args.device)
+        self.lstm2 = nn.LSTM(
             input_size=args.embedding_dim,
             hidden_size=self.hidden_dim,
-            num_layers=args.n_layers,
+            num_layers=1,
             batch_first=True
-        )
-        self.output_layer = nn.Linear(self.hidden_dim, n_features)
+        ).to(args.device)
+        self.output_layer = nn.Linear(self.hidden_dim, n_features).to(args.device)
 
     def forward(self, x):
-        # x shape (batch_size, embedding_dim)
-        x = x.unsqueeze(1).repeat(1, self.seq_len, 1)  # Добавляем измерение seq_len и повторяем x seq_len раз
-        x, (hidden_n, cell_n) = self.rnn1(x)
-        x, (hidden_n, cell_n) = self.rnn2(x)
-        x = x.reshape(-1, self.hidden_dim)  # Изменяем форму x на (batch_size * seq_len, hidden_dim)
-        x = self.output_layer(x)
-        return x.view(-1, self.seq_len, self.n_features)  # Возвращаем x в форме (batch_size, seq_len, n_features)
+        batch_size = x.size(0)
+        x = x.to(args.device)
+        # print(f'DECODER input dim: {x.shape}')
+        x = x.repeat_interleave(self.seq_len, dim=0)
+        # print(f'DECODER repeat dim: {x.shape}')
+        x = x.reshape((batch_size, self.seq_len, self.input_dim))
+        # print(f'DECODER reshaped dim: {x.shape}')
+        x, (hidden_n, cell_n) = self.lstm1(x)
+        # print(f'DECODER output rnn1 dim:/ {x.shape}')
+        x, (hidden_n, cell_n) = self.lstm2(x)
+        x = x.reshape((batch_size, self.seq_len, self.hidden_dim))
+        return self.output_layer(x)
 
 class LSTM_AUTO_ENCODER(nn.Module):
-    def __init__(self, seq_len, n_features):
+    def __init__(self, seq_len, n_features, args):
         super(LSTM_AUTO_ENCODER, self).__init__()
         self.seq_len = seq_len
         self.n_features = n_features
-        self.embedding_dim = args.embedding_dim  # Добавляем атрибут embedding_dim
-
-        self.encoder = Encoder(seq_len, n_features).to(args.device)
-        self.decoder = Decoder(seq_len, n_features).to(args.device)
+        self.encoder = Encoder(seq_len, n_features, args)
+        self.decoder = Decoder(seq_len, n_features, args)
 
     def forward(self, x):
         x = self.encoder(x)
-        x = x.view(-1, self.embedding_dim)  # Изменяем форму x на (batch_size, embedding_dim)
         x = self.decoder(x)
         return x
+
 
 seed = 42
 np.random.seed(seed)
@@ -128,24 +152,23 @@ torch.manual_seed(seed)
 parser = argparse.ArgumentParser()
 args = parser.parse_args('')
 
-
+args.device = torch.device('cpu')
 
 # ===== data loading ==== #
-args.batch_size = 4
+args.batch_size = 1
 
 # ==== model capacity ==== #
 args.n_layers = 1
-args.embedding_dim = 128
+args.embedding_dim = 256
 
-args.device=torch.device('cpu')
 
 # ==== regularization ==== #
-args.dropout = 0
-args.use_bn = False
+# args.dropout = 0  # Установка значения dropout
+args.use_bn = False  # batch normalization
 
 # ==== optimizer & training  # ====
 args.lr = 0.001
-args.epoch = 180
+args.epoch = 100
 ####################################################################
 #Предсказание модели
 ####################################################################
@@ -154,12 +177,12 @@ args.epoch = 180
 
 def predict(model, dataset):
     predictions, losses = [], []
-    criterion = nn.L1Loss(reduction='sum').to(args.device)
+    criterion = nn.MSELoss(reduction='sum').to(args.device)
 
     with torch.no_grad():
         model = model.eval()
         for seq_true in dataset:
-            seq_true = seq_true.to(args.device)
+            seq_true = seq_true.to(device=args.device)
             seq_pred = model(seq_true)
 
             loss = criterion(seq_pred, seq_true)
@@ -167,7 +190,6 @@ def predict(model, dataset):
             predictions.append(seq_pred.cpu().numpy().flatten())
             losses.append(loss.item())
     return predictions, losses
-
 ####################################################################
 #здесь веб-приложение
 ####################################################################
@@ -194,11 +216,52 @@ def interactiveplot(data):
     
 def threshold_plot(score):
     sns.set(style='dark')
-    score.plot(logy=True, figsize=(16,9), ylim=[1e-2, 1e2], color=['blue','red'],linewidth=2.5)
+    score.plot(logy=True, figsize=(16,9), color=['blue','red'],linewidth=2.5)
     # plt.savefig('score_plot.png')
     plt.grid()
     
-    
+def preds_plot(data_loader, model, columns):
+    combo_data_np = torch.cat([batch for batch in data_loader], dim=0).detach().cpu().numpy()
+
+    # Перестроить данные в подходящий формат для построения графиков
+    combo_data_np = combo_data_np.reshape(-1, seq_len, n_features)
+
+    # Получить предсказания модели
+    preds = []
+    with torch.no_grad():
+        model = model.eval()
+        for seq_true in data_loader:
+            seq_true = seq_true.to(args.device)
+            seq_true = seq_true.reshape((-1, seq_len, n_features))
+
+            seq_pred = model(seq_true)
+            preds.append(seq_pred.cpu().numpy())
+    preds = np.concatenate(preds, axis=0)
+
+    # Создать подграфики для каждой фичи
+    fig, axs = plt.subplots(n_features, 1, figsize=(10, n_features * 3))
+
+    # Построить график для каждой фичи
+    for i in range(n_features):
+        line1, = axs[i].plot(combo_data_np[:, 0, i], color='blueviolet', label='original data')
+        line2, = axs[i].plot(preds[:, 0, i], color='orange', label='predictions')
+        for j in range(1, seq_len):
+            axs[i].plot(combo_data_np[:, j, i], color='blueviolet')
+            axs[i].plot(preds[:, j, i], color='orange')
+        
+        # Использовать названия колонн вместо номеров фич
+        axs[i].set_title(columns[i])
+        axs[i].legend(handles=[line1, line2])
+
+    plt.tight_layout()
+    plt.show()
+
+
+
+
+
+
+   
 st.title("DPump")
 st.markdown('## Искуственный интеллект в предсказании выбытия УЭЦН ')
 st.markdown('Модель решает задачу детектирования аномалий во временных рядах')
@@ -212,53 +275,47 @@ image = 'anomalies_and_clean_data.png'
 # Отображение изображения
 st.image(image, caption='График аномалий и чистых данных', use_column_width=True)
 st.markdown('Наша нейросеть позволяет детектировать аномалии исходя из данных телеметрии')
-st.markdown('Физические процессы и датчики- основа в отобре параметров')
-st.markdown('Ввиду особенностей модели, возможны ложные сробатывания при изменении ваших показателей')
+st.markdown('Ввиду особенностей модели, возможны ложные срабатывания при изменении ваших показателей')
 
 st.markdown('**Метрики качества модели:**')
-metrics=pd.read_csv('metrics.csv')
+metrics=pd.read_csv('metrics_test_fft_mse_seq=1.csv')
 st.write(metrics) 
-st.markdown('ВНИМАНИЕ: данные, передаваемые в модель должны быть без пропусков')
+
 
 st.sidebar.title('Навигация')
 
 uploaded_file=st.sidebar.file_uploader('Загрузите ваш файл')
 
-options= st.sidebar.radio('Страницы ',
+options= st.sidebar.radio('Опции:',
 options=['Главная',
-'Данные', 
 'Статистика',
 'Интерактивный график данных',
-'Данные для модели',
 'Получить предсказание',
 ])
 if uploaded_file:
     #считываем данные 
+    delimiter_option = st.selectbox(
+        'Выберите разделитель для вашего csv-файла:',
+        options=[',', ';', '\t', '|', ' '],  # Список возможных разделителей
+        index=0  # Индекс выбранного по умолчанию разделителя
+    )
 
-    data=pd.read_csv(uploaded_file, low_memory=False)
-
+    # Чтение данных с выбранным разделителем
+    data = pd.read_csv(uploaded_file, low_memory=False, delimiter=delimiter_option)
+    st.markdown('**Ваши данные**')
+    show_data(data) 
         # Получить seq_len и n_features из первого элемента dataset
 
 
-    
+       
 
-
-
-    # строим фигурку
-        
-    if options == 'Данные':   
-        st.markdown('**Ваши данные**')
-        show_data(data) 
-    elif options =='Статистика':
+    if options =='Статистика':
         st.markdown('**Ваша статистика**')
         stats(data)
     elif options =='Интерактивный график данных':
         st.markdown('**Построение графиков на основе данных**')
         interactiveplot(data)            
-    elif options == 'Данные для модели': 
-        dataset, seq_len, n_features, df=create_dataset(data)  
-        st.markdown('**Данные для модели**')
-        show_data(df)   
+ 
    
     # elif options =='Гистограмма функции потерь':
     #     st.markdown('**Изменение функции потерь со временем**')
@@ -267,15 +324,21 @@ if uploaded_file:
     # elif options == 'Скоринг':   
   
     elif options =='Получить предсказание':
-
+        st.markdown('ВНИМАНИЕ: данные, передаваемые в модель должны быть без пропусков')
         st.markdown('**график детектирования аномалий**')
-        dataset, seq_len, n_features, df=create_dataset(data)
-
-        model = torch.load('DPump.pth', map_location=torch.device('cpu'))
+        data_fft, columns=prepare_data(data)  
+        data_to_tensor = Create_dataset(data_fft)
+        data_loader=data_to_tensor.dataset
+        seq_len = data_to_tensor.seq_len
+        n_features = data_to_tensor.n_features
+        model = torch.load('lstmae_mse_fft_seq=1.pth')
         model = model.to(args.device)
-        predictions, losses=predict(model, dataset)
-        Threshold = st.slider('Пороговое значение', min_value=0.0, max_value=1.0, value=0.03, step=0.01)
-        score = pd.DataFrame(index = data.index)
+        
+        predictions, losses=predict(model, data_loader)
+        st.markdown('**Выберите пороговое значение**')
+        Threshold = st.slider('Пороговое значение', min_value=0.0, max_value=1.0, value=0.0005, step=0.0002, format='%.4f')
+        Threshold = st.number_input('Введите пороговое значение вручную', min_value=0.0, max_value=1.0, value=Threshold, format='%.4f')
+        score = pd.DataFrame()
         score['Loss'] = losses
         score['Threshold'] = Threshold
         score['Anomaly'] = score['Loss'] > score['Threshold']
@@ -285,4 +348,7 @@ if uploaded_file:
         # st.set_option('deprecation.showPyplotGlobalUse', False)
         st.markdown('**результат детектирования аномалий**')
         
-        show_data(score)   
+        show_data(score) 
+        st.markdown('**Восстановленные данные**')  
+        
+        st.pyplot(preds_plot(data_loader, model, columns)) 
